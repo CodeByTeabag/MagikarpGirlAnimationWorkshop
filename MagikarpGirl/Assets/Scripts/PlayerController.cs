0259d79b60f0bb4bc21409a8eca71729e41ca85b
Davy de Bruin <davydb024@gmail.com>
2025-10-29 14:20:54 +0100

    Added PushUp Player Control

--- MagikarpGirl/Assets/Scripts/PlayerController.cs
+++ MagikarpGirl/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,44 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     
+    private CharacterController _controller;
     public Animator animator;
-    [SerializeField] private float moveSpeed = 5f;
     private Vector2 _moveInput;
+    
+    [Header("Player movement settings")] [SerializeField]
+    private float moveSpeed = 15f;
+    [SerializeField] private float turnSpeed = 90f;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
     
     void Update()
     {
-        Vector3 movement = new Vector3(_moveInput.x, 0f, _moveInput.y);
-        transform.Translate(movement * (moveSpeed * Time.deltaTime), Space.World);
+        Vector3 movement = transform.TransformDirection(_moveInput.x, 0, _moveInput.y);
+        _controller.Move(movement * (moveSpeed * Time.deltaTime));
+
         
-        animator.SetFloat("MoveX", movement.magnitude);
-        animator.SetFloat("MoveY", movement.magnitude);
-    }
+        float mouseX = Mouse.current.delta.x.ReadValue();
+        transform.Rotate(Vector3.up, mouseX * turnSpeed * Time.deltaTime);
 
-
-    public void OnMove(InputValue value)
-    {
-        _moveInput = value.Get<Vector2>();
+        animator.SetFloat("MoveX", _moveInput.x, 0.05f, Time.deltaTime);
+        animator.SetFloat("MoveY", _moveInput.y, 0.05f, Time.deltaTime);
+   
     }
     
+    public void OnMove(InputValue value)
+    {
+        if (!animator.GetBool("isMoving"))
+        {
+            _moveInput = value.Get<Vector2>();
+        }
+    }
+    
+    public void OnPushUp(InputValue value)
+    {
+        animator.SetTrigger("PushUpTrigger");
+        animator.SetBool("isMoving", false);
+    }
 }
